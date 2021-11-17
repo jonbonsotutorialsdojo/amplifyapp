86ebc94d3826a4ca9308757d11edf9fbc55d61d0
Tutorials Dojo <tutorialsdojo@Jons-MacBook-Pro.local>
2021-11-17 13:18:30 +0800

    modified the logo

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello from V2</h1>
+        <img src="https://stackleague.com/1ad624dae35d63a2781e501ca7bc826f.png" className="App-logo" alt="logo" />
+        <h1>Hello from StackLeague</h1>
       </header>
     </div>
   );
