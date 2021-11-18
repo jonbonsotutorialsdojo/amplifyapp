cce3056423a11f9bb4f6f156d7188844d85afeb9
Tutorials Dojo <tutorialsdojo@Jons-MacBook-Pro.local>
2021-11-18 12:19:04 +0800

    added auth

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header>
         <img src="https://stackleague.com/1ad624dae35d63a2781e501ca7bc826f.png" className="App-logo" alt="logo" />
-        <h1>Hello from StackLeague</h1>
+        <h1>We now have Auth!</h1>
       </header>
+      <AmplifySignOut />
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
