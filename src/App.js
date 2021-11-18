32c6ff102e274afc6d8f099a08e4238e5e5f19b8
Tutorials Dojo <tutorialsdojo@Jons-MacBook-Pro.local>
2021-11-18 11:44:20 +0800

    Updated CSS

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src="https://stackleague.com/1ad624dae35d63a2781e501ca7bc826f.png" width="600" height="600" className="App-logo" alt="logo" />
+        <img src="https://stackleague.com/1ad624dae35d63a2781e501ca7bc826f.png" className="App-logo" alt="logo" />
         <h1>Hello from StackLeague</h1>
       </header>
     </div>
